35373639467c475df718881743d3da3cc0b2d947
a1shuvo <a1shuvosaha@gmail.com>
2025-03-07 15:30:36 +0600

    Completed console log, string and boolean practice

--- variable/practice.js
+++ variable/practice.js
@@ -1,4 +1,8 @@
-// Practice - 1 to 7 (Page: 35)
+/* 
+* Topic: Variable
+* Page: 35
+* Practice: 1 to 7
+*/
 
 // #1
 const birthYear = 1993;
@@ -22,3 +26,124 @@ const studyHours = 8;
 const total = 36;
 
 console.log(birthYear, siblingsCount, students, marks, rooms, studyHours, total);
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+
+
+/* 
+* Topic: Console Log
+* Page: 38
+* Practice: 1 to 7
+*/
+
+// #1
+const age = 31;
+console.log(age);
+
+// #2
+const booksToRead = 5;
+console.log(booksToRead);
+
+// #3
+wakeUpTime = 9; // intentionally declared without keywords (global vaiable: bad practice)
+wakeUpTime = 7;
+console.log(wakeUpTime);
+
+// #4
+const wastedTime = 5;
+console.log(wastedTime);
+
+// #5
+const borrow = 0;
+console.log(borrow);
+
+// #6
+const goals = 3;
+console.log(goals);
+
+// #7
+const pillow = 2;
+console.log(pillow);
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+
+
+/* 
+* Topic: String
+* Page: 41
+* Practice: 1 to 7
+*/
+
+// #1
+const name = "Shuvo Saha";
+console.log(name);
+
+// #2
+const address = "Phulpur, Mymensingh";
+console.log(address);
+
+// #3
+const university = "Daffodil International University";
+console.log(university);
+
+// #4
+const fan = "Sai Pallavi";
+console.log(fan);
+
+// #5
+const movieOfTheYear = "Srikanth";
+console.log(movieOfTheYear);
+
+// #6
+const hobby = "Reading or Learning";
+console.log(hobby);
+
+
+// #7
+const color = "Sky Blue";
+console.log(color);
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+
+
+/* 
+* Topic: Boolean
+* Page: 42-43
+* Practice: 1 to 7
+*/
+
+// #1
+const isFriendHappy = false;
+console.log(isFriendHappy);
+
+// #2
+const isCrushMsg = true;
+console.log(isCrushMsg);
+
+// #3
+const celebrating = true;
+console.log(celebrating);
+
+// #4
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+// #5
+const isStudydone = false;
+console.log(isStudydone);
+
+// #6
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+// #7
+const isDinnerDone = false;
+console.log(isDinnerDone);
