470c9a5362c417f6c1240edb31d524afd7a84833
a1shuvo <a1shuvosaha@gmail.com>
2025-03-08 13:29:51 +0600

    Completed typeof practice

---
+++ data-type/practice.js
@@ -0,0 +1,29 @@
+/* 
+* Topic: Data Type
+* Page: 50
+* Practice: 1 to 6
+*/
+
+// #1
+const price = 555;
+console.log(typeof price);
+
+// #2
+const learning = true;
+console.log(typeof learning);
+
+// #3
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+// #4
+const temperature = 102.5;
+console.log(typeof temperature);
+
+// #5
+const isHungry = true;
+console.log(typeof isHungry);
+
+// #6
+const isRich = true;
+console.log(typeof isRich);
