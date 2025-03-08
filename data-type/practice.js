4c6c06eaab67b443afd50e27945b5a26ed16009c
a1shuvo <a1shuvosaha@gmail.com>
2025-03-08 13:44:16 +0600

    Completed Let Practice

--- data-type/practice.js
+++ data-type/practice.js
@@ -27,3 +27,52 @@ console.log(typeof isHungry);
 // #6
 const isRich = true;
 console.log(typeof isRich);
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+
+
+/* 
+* Topic: Let
+* Page: 52-53
+* Practice: 1 to 7
+*/
+
+// #1
+let age = 31;
+console.log(age);
+age = 25;
+console.log(age);
+
+// #2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+// #3
+let studyTime = 4;
+studyTime = 6;
+console.log(studyTime);
+
+// #4
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount);
+
+// #5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+// #6
+let chairsCount = 40;
+chairsCount = chairsCount - 2;
+chairsCount = chairsCount - 6;
+console.log(chairsCount);
+
+// #7
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
