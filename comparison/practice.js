57bd0fdb4d6c3adb2d92e2aebf677906f438befc
a1shuvo <a1shuvosaha@gmail.com>
2025-03-09 14:05:50 +0600

    Completed Comparison Practice

---
+++ comparison/practice.js
@@ -0,0 +1,36 @@
+/* 
+* Topic: Comparison
+* Page: 68
+* Practice: 1 to 7
+*/
+
+// #1
+const mySandwitch = 7;
+const brotherSandwitch = 5;
+console.log(mySandwitch > brotherSandwitch);
+
+// #2
+const myClassStudents = 45;
+const nextClassStudents = 50;
+console.log(myClassStudents > nextClassStudents);
+
+// #3
+const myScore = 75;
+const friendScore = 75;
+console.log(myScore == friendScore);
+
+// #4
+const targetMoney = 100;
+const savedMoney = 95;
+console.log(savedMoney >= targetMoney);
+
+// #5
+console.log(10 != 7);
+
+// #6
+console.log(20 <= 15);
+
+// #7
+const myResult = 45;
+const friendResult = 97;
+console.log(friendResult < myResult);
