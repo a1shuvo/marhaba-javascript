4c532ad033c4dba4ab5047629dba7194cd9f4802
a1shuvo <a1shuvosaha@gmail.com>
2025-03-09 14:51:44 +0600

    Completed Variable Comparison Practice

--- comparison/practice.js
+++ comparison/practice.js
@@ -34,3 +34,55 @@ console.log(20 <= 15);
 const myResult = 45;
 const friendResult = 97;
 console.log(friendResult < myResult);
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+
+
+/* 
+* Topic: Variable Comparison
+* Page: 70-71
+* Practice: 1 to 8
+*/
+
+// #1
+const brotherAge = 10;
+const myAge = 15;
+console.log("My younger brother's age is less than or equal to mine:", brotherAge <= myAge);
+
+// #2
+const passMarks = 33;
+const myMarks = 45;
+console.log("I have passed:", myMarks > passMarks);
+
+// #3
+const jamburaPrice = 10;
+const kodbelPrice = 40;
+console.log("Jambura is cheaper than Kodbel:", jamburaPrice < kodbelPrice);
+
+// #4
+const myReadBooks = 10;
+const friendReadBooks = 10;
+console.log("My friend and I read the same number of books:", myReadBooks == friendReadBooks);
+
+// #5
+const myBiscuits = 15;
+const friendBiscuits = 20;
+console.log("My biscuits are fewer than my friend's biscuits:", myBiscuits < friendBiscuits);
+
+// #6
+const RealMadridGoals = 2;
+const BarcelonaGoals = 3;
+console.log("Real Madrid scored fewer goals than Barcelona:", RealMadridGoals < BarcelonaGoals);
+
+// #7
+const myName = "Rahim";
+const friendName = "Karim";
+console.log("My name and my friend's name are the same:", myName == friendName);
+
+// #8
+const didStudy = true;
+const isFriendStudy = false;
+console.log("My friend and I studied today:", didStudy && isFriendStudy);
+
