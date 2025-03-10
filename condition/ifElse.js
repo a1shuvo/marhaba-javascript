c930109746f2cc684b424b3a7f15bd90907c2fc2
a1shuvo <a1shuvosaha@gmail.com>
2025-03-10 15:33:13 +0600

    Completed If-Else with Logical Operator Practices

--- condition/ifElse.js
+++ condition/ifElse.js
@@ -52,3 +52,98 @@ if (temperature >= 30) {
 } else {
     console.log("Sleep with a Blanket!");
 }
+
+
+
+
+/* --------------------------------------------------------------------------------------------------- */
+
+
+
+/* 
+* Topic: If-Else Condition with Logical Operators
+* Page: 80
+* Practice: 1 to 9
+*/
+
+// #1
+const age = 20;
+const height = 58;
+if (age > 18 && height > 60) {
+    console.log("Gari Thelbe!");
+} else {
+    console.log("Garite Bose Thakbe!");
+}
+
+// #2
+const mathScore = 90;
+const englishScore = 80;
+if (mathScore > 80 || englishScore > 85) {
+    console.log("Eligible for Admission!");
+} else {
+    console.log("You will get married!");
+}
+
+// #3
+const studentGPA = 5;
+const familyMonthlyIncome = 9999;
+if (studentGPA === 5 && familyMonthlyIncome < 10000) {
+    console.log("You will get a scholarship!"); 
+} else {
+    console.log("You have to pay a fee to study!");
+}
+
+// #4
+const candidateAge = 29;
+const workExperience = 3;
+if (candidateAge < 30 && workExperience > 2) {
+    console.log("You are eligible for the job exam!");
+} else {
+    console.log("You are not eligible for the job exam!");
+    
+}
+
+// #5
+const eggCount = 8;
+const haveChicken = false;
+if (eggCount > 12 || !haveChicken) {
+    console.log("Cook Egg Korma!");
+} else {
+    console.log("Eat Bread and Banana!");
+}
+
+// #6
+const bodyTemperature= 90;
+const haveCaugh = true;
+if (bodyTemperature > 100 || haveCaugh) {
+    console.log("Go to the doctor!");
+} else {
+    console.log("Lie down with a blanket!");
+}
+
+// #7
+const presentPercentage = 80;
+const submittedHomeWork = true;
+if (presentPercentage > 80 && submittedHomeWork) {
+    console.log("Eligible for Exam!");
+} else {
+    console.log("Auto Fail!!!");
+}
+
+// #8
+const haveElectricity = false;
+const haveChargeInMobile = false;
+if (!haveElectricity && !haveChargeInMobile) {
+    console.log("Study Books!");
+} else {
+    console.log("Play Video Games!");
+}
+
+// #9
+const shirtPrice = 1500;
+const haveCoupon = true;
+if (shirtPrice > 1000 && haveCoupon) {
+    console.log("You will get 20% Discount!");
+} else {
+    console.log("You will not get any Discount!");
+}
