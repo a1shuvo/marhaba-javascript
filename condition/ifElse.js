d0e3042215743e4700a673d30926cacba5a50c9f
a1shuvo <a1shuvosaha@gmail.com>
2025-03-10 14:37:21 +0600

    Completed If-Else Practices

---
+++ condition/ifElse.js
@@ -0,0 +1,54 @@
+/* 
+* Topic: If-Else Condition
+* Page: 77
+* Practice: 1 to 6
+*/
+
+// #1
+const homeArrivalTime = 6;
+if (homeArrivalTime < 6) {
+    console.log("Nasta Khabo!");
+} else {
+    console.log("Jharur Bari Khabo!!!");
+}
+
+// #2
+const isLoggedIn = true;
+if (isLoggedIn) {
+    console.log("Welcome!");
+} else {
+    console.log("Get Lost!");
+}
+
+// #3
+const runInKM = 3;
+if (runInKM === 5) {
+    console.log("Get Chocolate!");
+} else {
+    console.log("Get Mota Vuri!");
+}
+
+// #4
+const myScore = 85;
+if (myScore > 80) {
+    console.log("Get a Bike!");
+} else {
+    console.log("Don't Get a Bike!");
+}
+
+// #5
+const movieShowTime = 10;
+if (movieShowTime < 9) {
+    console.log("Watch the Movie!");
+} else {
+    console.log("Sleep with oil in nose!");
+}
+
+// #6
+const temperature = 30;
+if (temperature >= 30) {
+    console.log("Turn on the Air Conditioner!");
+    
+} else {
+    console.log("Sleep with a Blanket!");
+}
