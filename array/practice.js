0124442096656976ce275719b1c61989afaf1fde
a1shuvo <a1shuvosaha@gmail.com>
2025-03-13 11:57:55 +0600

    Completed Array declaration & length practices

---
+++ array/practice.js
@@ -0,0 +1,30 @@
+/* 
+* Topic: Array declaration & length
+* Page: 93
+* Practice: 1 to 7
+*/
+
+
+// #1
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+// #2
+const fruits = ["apple", "banana", "orange", "mango", "guava"];
+
+// #3
+const vegetables = ["alu", "peyaj", "begun", "mula", "gajor", "tomato", "dheros", "lau", "sosa", "mula"];
+console.log(vegetables.length);
+
+// #4
+const movies = ["Star Wars", "PK", "Tare Zameen Par", "Monpura", "Srabon Megher Din"];
+
+// #5
+const oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+// #6
+const colors = ["red", "green", "orange", "blue", "pink", "purple", "white"];
+console.log(colors.length);
+
+// #7
+const capitals = ["Dhaka", "Baku", "Vienna", "Kabul", "Beijing", "Cairo", "Paris", "Berlin", "Tokyo"];
+console.log(capitals.length);
