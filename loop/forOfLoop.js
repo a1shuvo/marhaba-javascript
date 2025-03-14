3821baf1a58d23ff2c7d1060ad6eef2ce4e49334
a1shuvo <a1shuvosaha@gmail.com>
2025-03-14 17:13:50 +0600

    Completed For of Loop Practices

---
+++ loop/forOfLoop.js
@@ -0,0 +1,48 @@
+/* 
+* Topic: For Of Loop
+* Page: 105-106
+* Practice: 1 to 5
+*/
+
+
+// #1
+const subjects = ["Math", "English", "Physics", "Computer Science", "Data Science"];
+for (const subject of subjects) {
+    console.log(subject);
+}
+
+// #2
+const foods = ["Biriyani", "Mutton Rezala", "Chicken Masala", "Curd", "Prawn Fried Rice"];
+for (const item of foods) {
+    console.log(item);
+}
+
+// #3
+const birthYears = [1993, 2000, 1971, 1985, 1999, 2023]
+for (const year of birthYears) {
+    console.log(year);
+}
+
+// #4
+const teamBangladesh = [
+    "Shakib Al Hasan",
+    "Tamim Iqbal",
+    "Mushfiqur Rahim",
+    "Mahmudullah Riyad",
+    "Mustafizur Rahman",
+    "Sabbir Rahman",
+    "Litton Das",
+    "Mohammad Saifuddin",
+    "Mehidy Hasan Miraz",
+    "Nasum Ahmed",
+    "Taskin Ahmed"
+];
+for (const player of teamBangladesh) {
+    console.log(player);
+}
+
+// #5
+const examDates = ["10-04-2025", "12-04-2025", "15-04-2025", "18-04-2025", "20-04-2025"];
+for(const date of examDates){
+    console.log(date);
+}
