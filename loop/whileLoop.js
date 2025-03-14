b3f5c643d287277126c51ea081291520411477b4
a1shuvo <a1shuvosaha@gmail.com>
2025-03-14 17:42:17 +0600

    Completed While Loop Practices

---
+++ loop/whileLoop.js
@@ -0,0 +1,41 @@
+/* 
+* Topic: While Loop
+* Page: 109
+* Practice: 1 to 5
+*/
+
+
+// #1
+let count = 0;
+while (count < 10) {
+    console.log("I will do 3 hours of programming practice every day");
+    count++;
+}
+
+// #2
+let num = 1;
+while (num <= 20) {
+    console.log(num);
+    num++;
+}
+
+// #3
+let number = 50;
+while (number <= 100) {
+    console.log(number);
+    number++;
+}
+
+// #4
+let i = 1;
+while (i <= 10) {
+    console.log("3 x", i, "=", i*3);
+    i++;
+}
+
+// #5
+let x = 111;
+while (x >= 110) {
+    console.log(x/2);
+    x--;
+}
