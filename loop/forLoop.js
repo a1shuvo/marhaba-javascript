140d04be40d428a64b569857949487d89c8c39a6
a1shuvo <a1shuvosaha@gmail.com>
2025-03-16 16:37:58 +0600

    Completed For Loop Practices

---
+++ loop/forLoop.js
@@ -0,0 +1,27 @@
+/* 
+* Topic: For Loop
+* Page: 114
+* Practice: 1 to 3
+*/
+
+
+// #1
+for (let i = 150; i <= 170; i++) {
+    console.log(i);   
+}
+
+// #2
+let sum = 0;
+for (let i = 31; i <= 58; i++) {
+    sum += i;
+}
+
+console.log("Sum of Numbers 31 to 58 is:", sum);
+
+// #3
+let sumOfNumbers = 0;
+for (let i = 25; i <= 75; i++) {
+    sumOfNumbers += i;
+}
+
+console.log("Sum of Numbers 25 to 75 is:", sumOfNumbers);
