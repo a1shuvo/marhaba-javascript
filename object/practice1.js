f3d1d8e8fdd55affe1984f39b2eebca356fa7efe
a1shuvo <a1shuvosaha@gmail.com>
2025-03-17 20:09:01 +0600

    Completed Object Declaration Practices

---
+++ object/practice1.js
@@ -0,0 +1,66 @@
+/* 
+* Topic: Object Declaration or Creation
+* Page: 126-127
+* Practice: 1 to 7
+*/
+
+
+// #1
+const object = {
+    name: "Ajith Sir",
+    subject: "Math",
+    age: 75,
+    homeAddress: "Phulpur, Mymensingh",
+    isRetired: true
+}
+
+// #2
+const tree = {
+    name: "Mango Tree",
+    height: "20 Feet",
+    age: 5,
+    fruitName: "Mango"
+}
+
+// #3
+const cat = {
+    name: "Mini",
+    color: "White",
+    age: 2,
+    isSleeping: false
+}
+
+// #4
+const father = {
+    name: "Jantu Saha",
+    age: 54,
+    occupation: "Business",
+    childCount: 2,
+    childNames: ["Shuvo", "Rudra"]
+}
+
+// #5
+const motorbike = {
+    brand: "Hero",
+    tyreCount: 2,
+    color: "Red",
+    highestSpeed: 150,
+    price: 120000
+}
+
+// #6
+const bird = {
+    name: "Parrot",
+    color: "Green",
+    foods: ["Coconut", "Guava", "Chili", "Rice", "Carrot"]
+}
+
+// #7
+const laptop = {
+    brand: "Lenevo",
+    processor: "Intel Core i5",
+    ram: "16 GB",
+    display: "15 Inch",
+    color: "Black",
+    price: 35000
+}
