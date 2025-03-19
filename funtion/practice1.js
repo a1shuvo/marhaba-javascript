16040296b1ecafe09c4a033b2a2c84a822d2d1c0
a1shuvo <a1shuvosaha@gmail.com>
2025-03-19 17:11:28 +0600

    Completed Function Declaration & Calling Practices

---
+++ funtion/practice1.js
@@ -0,0 +1,67 @@
+/* 
+* Topic: Function Declaration & Calling
+* Page: 143
+* Practice: 1 to 7
+*/
+
+
+// #1
+function juiceKhao() {
+    console.log("Ei nao lebur juice!");
+}
+juiceKhao();
+
+
+// #2
+function myRoutine() {
+    console.log("Brush Teeth.");
+    console.log("Drink Water.");
+    console.log("Go to Washroom.");
+    console.log("Do Exercise.");    
+    console.log("Eat Breakfast.");
+    console.log("Do Programming!");
+}
+myRoutine();
+
+
+// #3
+function myFamilyMembers() {
+    console.log("Baba");
+    console.log("Ma");
+    console.log("Vai");
+    console.log("Srija");
+    console.log("Srijar Ma");
+}
+myFamilyMembers();
+
+
+// #4
+function taskDone() {
+    console.log("Exercise");
+    console.log("Programming");
+    console.log("Reading Books");
+    console.log("Eating");
+    console.log("Watching Youtube");
+}
+taskDone();
+
+
+// #5
+function greetMe() {
+    console.log("Good Morning! Have a great day!");
+}
+greetMe();
+
+
+// #6
+function getFavFriends() {
+    console.log("Ankur", "\nAnindya", "\nTuhin");
+}
+getFavFriends();
+
+
+// #7
+function introducMyself() {
+    console.log("Shuvo Saha", "\n31", "\nMymensingh, Phulpur", "\n01725xxxxxx", "\n5 Feet 6 Inch", "\nBiriyani");
+}
+introducMyself();
