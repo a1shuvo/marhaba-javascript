abd3b273bd7c23532450e11171d03d36fca36856
a1shuvo <a1shuvosaha@gmail.com>
2025-03-28 23:49:11 +0600

    Completed String Method repeat, replace and replaceAll Practices

---
+++ string-method/practice4.js
@@ -0,0 +1,20 @@
+/* 
+* Topic: String Method (repeat(), toString(), replace(), replaceAll())
+* Page: 250
+* Practice: 1 to 3
+*/
+
+
+// #1
+const greet = 'hi'
+console.log(greet.repeat(10));
+
+
+// #2
+const text = 'I am learning JavaScript, JavaScript is amazing!'
+console.log(text.replace('JavaScript', 'JS'));
+
+
+// #3
+const txt = 'I love apples and apples are tasty!';
+console.log(txt.replaceAll('apples', 'mangoes'));
