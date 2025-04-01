a3264286a599d19a1ce0d1c50cce87acabf52e15
a1shuvo <a1shuvosaha@gmail.com>
2025-04-01 20:00:07 +0600

    Completed JS Errors Practices

---
+++ error-validation/practice2.js
@@ -0,0 +1,40 @@
+/* 
+* Topic: Error Validation (JS Errors)
+* Page: 301
+* Practice: 1 to 3
+*/
+
+
+/* 
+* Different types of Error in JavaScript:
+* Syntax Error
+* TypeError
+* Reference Error
+* Logical Error
+* Range Error
+*/ 
+
+
+// #1
+/* 
+* No semicolon usually doesn't cause an immediate error due to Automatic Semicolon Insertion (ASI).
+* But it can result in unexpected behavior or logical issues.
+* Syntax errors can happen when the absence of semicolons leads to ambigu ities in the code. 
+*/
+
+
+// #2
+/* 
+* A reference error occurs when JavaScript encounters a variable that has not been declared.
+* Trying to access a variable that hasn't been initialized or declared in the current scope.
+*/
+
+
+// #3
+/* 
+* Differce between Syntax Error and Type Error:
+* Syntax errors happen when we write invalid JavaScript code. For example: missing parentheses, incorrect use of keywords, or unbalanced curly braces.
+* Type errors happen when we try to invoke a method or perform an operation on a value that doesn't support that operation, such as calling a string method on a number.
+* Occurs when JavaScript can't parse or read the code because of invalid structure or grammar in the code.
+* Occurs when we try to do something with the wrong data type or performing an operation on an incompatible data type.
+*/
