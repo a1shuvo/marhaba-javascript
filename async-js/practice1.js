e2a2043524cbab7c9080670de73b1deae1349598
a1shuvo <a1shuvosaha@gmail.com>
2025-04-02 08:45:54 +0600

    Completed Single Threaded JS Practices

---
+++ async-js/practice1.js
@@ -0,0 +1,19 @@
+/* 
+* Topic: Async JS (single threaded)
+* Page: 303
+* Practice: 1 to 2
+*/
+
+
+// #1
+/* 
+* If we call a function inside synchronous code, JavaScript will execute the function after completing the previous code execution (if any). 
+* The function's output will be displayed, and then the next code will be executed sequentially.
+*/
+
+
+// #2
+/* 
+* JavaScript is a single-threaded programming language by nature. 
+* This means that JavaScript will execute the code line by line, maintaining a sequential order.
+*/
