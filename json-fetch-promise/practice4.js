392cb693aa73b533304dd0ca94088e3f7b09e8c1
a1shuvo <a1shuvosaha@gmail.com>
2025-04-05 20:22:45 +0600

    Completed CRUD Operation Practices

---
+++ json-fetch-promise/practice4.js
@@ -0,0 +1,43 @@
+/* 
+* Topic: JSON Fetch Promise (CRUD operation)
+* Page: 331
+* Practice: 1 to 5
+*/
+
+
+// #1
+/* 
+* If we want to change a part of any data, we can use 'PATCH' method. 
+* Like profile 'Single' or 'Married' for this type of data change we use 'PATCH' instead of 'PUT' method. 
+*/
+
+
+// #2
+/* 
+* If we want to add new information in database, we use 'POST' HTTP method.
+*/
+
+
+// #3
+/* 
+* When we see any video from a website, it is 'Read' operation or 'GET' HTTP method.
+*/
+
+
+// #4
+/* 
+* CRUD stands for Create, Read, Update, and Delete. 
+* These are the four basic operations that we perform on data in a database.
+*/
+
+
+// #5
+/* 
+* HTTP methods (or verbs) are used to specify the type of action the client wants to perform on a given resource when interacting with a web server. 
+* The most common HTTP methods are: GET, POST, PATCH, PUT, DELETE.
+* GET: Retrieves data from a server.
+* POST: Sends data to the server to create a new resource.
+* PATCH: Partially updates a resource, meaning it only modifies certain fields rather than replacing the entire resource.
+* PUT: Updates an existing resource or creates a new one if it doesn’t exist.
+* DELETE: Deletes a resource from the server.
+*/
